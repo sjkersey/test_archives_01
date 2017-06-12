0460393a91d17db5a8f17811575b0605867ba86a
Scott K <sjkersey@aol.com>
2017-06-12 16:42:27 -0400

    created /home/jenkins and moved all jenkins triggers and logs to there from /home/scott for security

--- test_Jenkins_trigger.sh
+++ test_Jenkins_trigger.sh
@@ -1,6 +1,8 @@
-# echo "-----------------------" >> /home/scott/Jenkins_triggers.log
-echo -n "testing Jenkins Trigger" >> /home/scott/Jenkins_triggers.log
-printf "%s" " --- " >> /home/scott/Jenkins_triggers.log 
-date | xargs echo -n >> /home/scott/Jenkins_triggers.log
-# printf "%s" "\n"
+# see https://superuser.com/questions/395668/how-do-i-echo-something-file-txt-without-carriage-return
+
+# echo "-----------------------" >> /home/jenkins/Jenkins_triggers.log
+echo -n "testing Jenkins Trigger" >> /home/jenkins/Jenkins_triggers.log
+printf "%s" " --- " >> /home/jenkins/Jenkins_triggers.log 
+date | xargs echo >> /home/jenkins/Jenkins_triggers.log
+# printf "%s\n" " -- " >> /home/jenkins/Jenkins_triggers.log
 
